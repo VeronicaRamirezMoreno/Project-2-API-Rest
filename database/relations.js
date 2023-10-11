3b276679d5d5a669234e1c4eb73ab2ba4deb327c
Guacimara Ramirez Santana <guaci.rs10@gmail.com>
2023-10-11 19:20:01 +0100

    Created Connection

---
+++ database/relations.js
@@ -0,0 +1,12 @@
+//Requerir modelos 
+
+
+function addRelationsToModels() {
+	try {
+		console.log('Relations added to all models')
+	} catch (error) {
+		throw error
+	}
+}
+
+module.exports = addRelationsToModels
---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const { checkConnection, syncModels } = require('./database/index')
+const addRelationsToModels = require('./database/relations')
+
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+
+async function checkAndSyncPostgreSQL() {
+	await checkConnection()
+	addRelationsToModels()
+	await syncModels('alter')
+}
+
+function initializeAndListenWithExpress() {
+	const app = express()
+		.use(cors())
+		.use(morgan('dev'))
+		.use(express.json())
+		//.use('/api', require('./api/routes'))
+
+		.listen(process.env.PORT, () => {
+			console.log(`> Listening on port: 3000`)
+		})
+}
+
+async function startAPI() {
+	await checkAndSyncPostgreSQL()
+	initializeAndListenWithExpress()
+}
+
+startAPI()
