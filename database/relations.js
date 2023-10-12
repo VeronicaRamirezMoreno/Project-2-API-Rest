6da36c57f6aa1078a96e3c6f3e00edfc6863d54c
Guacimara Ramirez Santana <guaci.rs10@gmail.com>
2023-10-12 16:13:01 +0100

    Relation User-Pet created

--- database/relations.js
+++ database/relations.js
@@ -4,6 +4,11 @@ const Pet = require ('../api/models/pets.models')
 
 function addRelationsToModels() {
 	try {
+		
+		//One to many
+		User.hasMany(Pet)
+		Pet.belongsTo(User)
+
 		console.log('Relations added to all models')
 	} catch (error) {
 		throw error
