0c1265102e6368edd3d4b0d27d74f1ba630bafcc
Vero <veroramirezmoreno@gmail.com>
2023-10-15 09:27:21 +0100

    contact_info route updated

--- api/routes/contact_info.route.js
+++ api/routes/contact_info.route.js
@@ -1,14 +1,20 @@
 const router = require('express').Router()
 const { checkAdmin, checkPersonnel } = require("../middlewares/auth");
 
-const { getAllContactInfo, getOwnContactInfo, getOneContactInfo, createContactInfo, updateContactInfo, deleteContactInfo } = require('../controllers/contact_info.contoller')
+const { 
+    getAllContactInfo,
+    getOwnContactInfo,
+    getOneContactInfo,
+    createContactInfo,
+    updateContactInfo,
+    deleteContactInfo } = require('../controllers/contact_info.contoller')
 
 router.get('/', checkPersonnel, getAllContactInfo)
 router.get('/profile', getOwnContactInfo)
 router.get('/:contactId', checkPersonnel, getOneContactInfo)
 router.post('/profile', createContactInfo)
 router.post('/', checkPersonnel, createContactInfo)
-router.put('/:contactId', checkPersonnel,updateContactInfo)
-router.delete('/:contactId', checkPersonnel,deleteContactInfo)
+router.put('/:contactId', checkPersonnel, updateContactInfo)
+router.delete('/:contactId', checkPersonnel, deleteContactInfo)
 
 module.exports = router
