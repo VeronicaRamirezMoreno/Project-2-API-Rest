171921f646544411c2af524b6eee50a4e55f5518
Vero <veroramirezmoreno@gmail.com>
2023-10-15 13:20:09 +0100

    createPet updated and add pet to user

--- api/routes/pets.route.js
+++ api/routes/pets.route.js
@@ -1,13 +1,19 @@
 const router = require('express').Router()
-const { checkAdmin , checkPersonnel} = require("../middlewares/auth");
+const { checkAdmin, checkPersonnel } = require("../middlewares/auth");
 
-const { getAllPets, getOnePet, createPet, updatePet,addPetToUser, deletePet } = require('../controllers/pets.controllers')
+const {
+    getAllPets,
+    getOnePet,
+    createPet,
+    updatePet,
+    addPetToUser,
+    deletePet } = require('../controllers/pets.controllers')
 
 router.get('/', checkPersonnel, getAllPets)
-router.get('/:petId',checkPersonnel, getOnePet)
-router.post('/', checkPersonnel,createPet)
-router.put('/:petId',checkPersonnel, updatePet)
-router.put('/:petId/:userId',checkPersonnel, addPetToUser)
-router.delete('/:petId', checkPersonnel,deletePet)
+router.get('/:petId', checkPersonnel, getOnePet)
+router.post('/:userId', createPet)
+router.put('/:petId', checkPersonnel, updatePet)
+router.put('/:petId/:userId', checkPersonnel, addPetToUser)
+router.delete('/:petId', checkPersonnel, deletePet)
 
 module.exports = router
