04a882a87315cca96461d69fd98f9958fc9f8a7e
Guacimara Ramirez Santana <guaci.rs10@gmail.com>
2023-10-16 04:21:01 +0100

    Crazyyyy Appoiment

--- api/controllers/appointment.controller.js
+++ api/controllers/appointment.controller.js
@@ -27,6 +27,22 @@ async function getOneAppointment(req, res) {
 		res.status(500).send(error.message)
 	}
 }
+async function getVetAppointments(req, res) {
+	try {
+		const appointments = await Appointment.findAll({
+	 	where: {
+				id: res.locals.user.id
+			},            
+		})
+		if (appointments) {
+			return res.status(200).json({appointments})
+		} else {
+			return res.status(404).send('Have not appointments available')
+		}
+	} catch (error) {
+		res.status(500).send(error.message)
+	}
+}
 
 async function createAppointment(req, res) {
 	try {
@@ -75,6 +91,7 @@ async function deleteAppointment(req, res) {
 module.exports = {
 	getAllAppointments,
 	getOneAppointment,
+	getVetAppointments,
 	createAppointment,
 	updateAppointment,
 	deleteAppointment
--- api/routes/appointment.route.js
+++ api/routes/appointment.route.js
@@ -1,10 +1,21 @@
 const router = require('express').Router()
+const { checkAdmin, checkPersonnel } = require("../middlewares/auth");
 
-const { getAllAppointments, getOneAppointment, createAppointment, updateAppointment, deleteAppointment } = require('../controllers/appointment.controller')
+const { getAllAppointments, 
+    getOneAppointment, 
+    getAvailableAppointments,
+    getVetAppointments,
+    createAppointment, 
+    updateAppointment, 
+    deleteAppointment 
+} = require('../controllers/appointment.controller')
 
-router.get('/', getAllAppointments)
-router.get('/:appointmentId', getOneAppointment)
-router.post('/', createAppointment)
+
+router.get('/',checkPersonnel, getAllAppointments)
+router.get('/:appointmentId', checkPersonnel, getOneAppointment)
+//router.get('/available', getAvailableAppointments)
+router.get('/profile', getVetAppointments)
+router.post('/', checkAdmin, createAppointment)
 router.put('/:appointmentId', updateAppointment)
 router.delete('/:appointmentId', deleteAppointment)
 
