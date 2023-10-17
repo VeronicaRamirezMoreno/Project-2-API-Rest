ec7edb1d4c7b7d74a6c280325a9d03bc0ec0728d
Vero <veroramirezmoreno@gmail.com>
2023-10-17 19:14:38 +0100

    DB connection

--- database/index.js
+++ database/index.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
-    host: 'localhost',
-    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    dialect: process.env.DIALECT,
     port: process.env.DB_PORT,
     logging: false,
 })
